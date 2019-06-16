4696f820ed553ef14ebfb6a42d7ce3f59304d068
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-16 16:43:15 +0000

    Working

--- difference-of-squares/lib/difference_of_squares.dart
+++ difference-of-squares/lib/difference_of_squares.dart
@@ -2,6 +2,10 @@ import 'dart:math' show pow;
 
 class DifferenceOfSquares {
   num squareOfSum(num input) => 1;
-  num sumOfSquare(num input) => 1;
+  num sumOfSquare(num n) {
+    // https://en.wikipedia.org/wiki/Square_pyramidal_number
+    return (pow(n, 3) / 3) + (pow(n, 2) / 2) + (n / 6);
+  }
+
   num differenceOfSquares(num input) => null;
 }
