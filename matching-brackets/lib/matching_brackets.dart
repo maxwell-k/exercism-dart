e3296c95b9c09e8a1d2557084dd249c5046c79f0
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-16 11:30:48 +0000

    Working

--- matching-brackets/lib/matching_brackets.dart
+++ matching-brackets/lib/matching_brackets.dart
@@ -1,6 +1,16 @@
 class MatchingBrackets {
   bool isPaired(String expression) {
     int square = 0;
+    for (var i = 0; i < expression.length; i++) {
+      switch (expression[i]) {
+        case '[':
+          square += 1;
+          break;
+        case ']':
+          square -= 1;
+          break;
+      }
+    }
     return square == 0;
   }
 }
