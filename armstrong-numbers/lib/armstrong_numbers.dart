24abc2f4287efa9e4b293122ab28c4a7871743fb
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-16 16:02:11 +0000

    Working

--- armstrong-numbers/lib/armstrong_numbers.dart
+++ armstrong-numbers/lib/armstrong_numbers.dart
@@ -1,6 +1,7 @@
 class ArmstrongNumbers {
   bool isArmstrongNumber(int number) {
-    assert(number.toString().length == 1, "Restricted to single digit numbers");
+    final digits = number.toString();
+    assert(digits.length == 1, "Restricted to single digit numbers");
     return true;
   }
 }
