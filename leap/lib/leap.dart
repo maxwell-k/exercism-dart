511d7d507a42676135e335b4514263ee58ae33ed
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-16 12:57:17 +0000

    Working

--- leap/lib/leap.dart
+++ leap/lib/leap.dart
@@ -1,5 +1,7 @@
 class Leap {
   bool leapYear(int year) {
+    if (year % 100 == 0) return false;
+    if (year % 4 == 0) return true;
     return false;
   }
 }
--- leap/test/leap_test.dart
+++ leap/test/leap_test.dart
@@ -13,29 +13,33 @@ void main() {
     test("is introduced every 4 years to adjust about a day", () {
       final bool result = leap.leapYear(1996);
       expect(result, equals(true));
-    }, skip: true);
+    }, skip: false);
 
     test("is skipped every 100 years to remove an extra day", () {
       final bool result = leap.leapYear(2100);
       expect(result, equals(false));
-    }, skip: true);
+    }, skip: false);
 
     test("is reintroduced every 400 years to adjust another day", () {
       final bool result = leap.leapYear(2000);
       expect(result, equals(true));
     }, skip: true);
 
-    test("is introduced every 4 years to adjust about a day before 400 A.D.", () {
+    test("is introduced every 4 years to adjust about a day before 400 A.D.",
+        () {
       final bool result = leap.leapYear(4);
       expect(result, equals(true));
     }, skip: true);
 
-    test("is skipped every 100 years to remove an extra day before 400 A.D.", () {
+    test("is skipped every 100 years to remove an extra day before 400 A.D.",
+        () {
       final bool result = leap.leapYear(300);
       expect(result, equals(false));
     }, skip: true);
 
-    test("is reintroduced every 400 years to adjust another day including 400 A.D.", () {
+    test(
+        "is reintroduced every 400 years to adjust another day including 400 A.D.",
+        () {
       final bool result = leap.leapYear(400);
       expect(result, equals(true));
     }, skip: true);
