7ba12fde9b8ad08c2b2d29dd40bf48a18d62104f
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-16 15:40:18 +0000

    Refactor to use an input table
    That more closely represents the README

--- scrabble-score/lib/scrabble_score.dart
+++ scrabble-score/lib/scrabble_score.dart
@@ -1,36 +1,21 @@
-final data = const {
-  'A': 1,
-  'B': 3,
-  'C': 3,
-  'D': 2,
-  'E': 1,
-  'F': 4,
-  'G': 2,
-  'H': 4,
-  'I': 1,
-  'J': 8,
+Map<String, int> table = const {
+  'A, E, I, O, U, L, N, R, S, T': 1,
+  'D, G': 2,
+  'B, C, M, P': 3,
+  'F, H, V, W, Y': 4,
   'K': 5,
-  'L': 1,
-  'M': 3,
-  'N': 1,
-  'O': 1,
-  'P': 3,
-  'Q': 10,
-  'R': 1,
-  'S': 1,
-  'T': 1,
-  'U': 1,
-  'V': 4,
-  'W': 4,
-  'X': 8,
-  'Y': 4,
-  'Z': 10,
+  'J, X': 8,
+  'Q, Z': 10,
 };
 
 int score(String word) {
-  var scores = <int>[];
+  Map<String, int> scores = {};
+  table.forEach((letters, score) =>
+      letters.split(', ').forEach((letter) => scores[letter] = score));
+
+  int score = 0;
   for (var i = 0; i < word.length; i++) {
-    scores.add(data[word[i].toUpperCase()]);
+    score += scores[word[i].toUpperCase()];
   }
-  return scores.fold(0, (total, i) => total + i);
+  return score;
 }
