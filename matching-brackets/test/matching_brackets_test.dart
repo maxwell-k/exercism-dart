41f11801e512c4874fbfa964cdce3901a01a9de8
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-16 11:26:30 +0000

    Reformat with dartfmt

--- matching-brackets/test/matching_brackets_test.dart
+++ matching-brackets/test/matching_brackets_test.dart
@@ -80,13 +80,14 @@ void main() {
     }, skip: true);
 
     test("math expression", () {
-      final bool result = matchingBrackets.isPaired("(((185 + 223.85) * 15) - 543)/2");
+      final bool result =
+          matchingBrackets.isPaired("(((185 + 223.85) * 15) - 543)/2");
       expect(result, equals(true));
     }, skip: true);
 
     test("complex latex expression", () {
-      final bool result = matchingBrackets
-          .isPaired("\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)");
+      final bool result = matchingBrackets.isPaired(
+          "\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)");
       expect(result, equals(true));
     }, skip: true);
   });
