fdd621172a52c867b6dac69ca70ab7c0dcccc0f5
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-16 12:59:39 +0000

    Working

--- leap/test/leap_test.dart
+++ leap/test/leap_test.dart
@@ -29,19 +29,19 @@ void main() {
         () {
       final bool result = leap.leapYear(4);
       expect(result, equals(true));
-    }, skip: true);
+    }, skip: false);
 
     test("is skipped every 100 years to remove an extra day before 400 A.D.",
         () {
       final bool result = leap.leapYear(300);
       expect(result, equals(false));
-    }, skip: true);
+    }, skip: false);
 
     test(
         "is reintroduced every 400 years to adjust another day including 400 A.D.",
         () {
       final bool result = leap.leapYear(400);
       expect(result, equals(true));
-    }, skip: true);
+    }, skip: false);
   });
 }
