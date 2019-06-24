1b905a6f111cffb8f5b962973e4461a434f4e106
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-24 17:40:27 +0000

    Working

--- bob/lib/bob.dart
+++ bob/lib/bob.dart
@@ -2,6 +2,7 @@ class Bob {
   bool yell(String prompt) =>
       prompt.contains(RegExp(r'[a-zA-Z]')) && prompt == prompt.toUpperCase();
   bool question(String prompt) => prompt.endsWith("?");
+  bool silence(String prompt) => prompt.isEmpty;
   String response(String prompt) {
     if (yell(prompt) && question(prompt)) {
       return "Calm down, I know what I'm doing!";
@@ -9,7 +10,7 @@ class Bob {
       return "Whoa, chill out!";
     } else if (question(prompt)) {
       return "Sure.";
-    } else if (prompt.isEmpty) {
+    } else if (silence(prompt)) {
       return "Fine. Be that way!";
     } else {
       return "Whatever.";
