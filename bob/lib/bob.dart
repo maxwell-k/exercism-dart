58bd9ede8b9f2fedd2510817890d4f0e58f4a7f8
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-24 17:49:33 +0000

    Refactor based on example solution

--- bob/lib/bob.dart
+++ bob/lib/bob.dart
@@ -3,8 +3,8 @@ class Bob {
       me.contains(RegExp(r'[a-zA-Z]')) && me == me.toUpperCase();
   bool question(String me) => me.endsWith("?");
   bool silence(String me) => me.isEmpty;
-  String response(String input) {
-    final me = input.trim();
+  String response(String message) {
+    final me = message.trim();
     if (yell(me) && question(me)) {
       return "Calm down, I know what I'm doing!";
     } else if (yell(me)) {
