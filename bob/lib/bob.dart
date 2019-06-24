d4ccf1d91730e48446035b58ea07bc7ee4feb187
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-24 17:27:14 +0000

    Working

--- bob/lib/bob.dart
+++ bob/lib/bob.dart
@@ -1,10 +1,11 @@
 class Bob {
   bool yell(String prompt) => prompt == prompt.toUpperCase();
+  bool question(String prompt) => prompt.endsWith("?");
   String response(String prompt) {
     /* return "Calm down, I know what I'm doing!"; } else */
     if (yell(prompt)) {
       return "Whoa, chill out!";
-    } else if (prompt.endsWith("?")) {
+    } else if (question(prompt)) {
       return "Sure.";
     } else {
       return "Whatever.";
