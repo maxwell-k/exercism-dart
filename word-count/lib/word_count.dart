0214067c3f8b6f43cdae5cd52b288c706c401d94
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-24 13:57:59 +0000

    Working

--- word-count/lib/word_count.dart
+++ word-count/lib/word_count.dart
@@ -1,8 +1,9 @@
 class WordCount {
   Map<String, int> countWords(String text) {
     Map<String, int> result = {};
-    text.split(RegExp(r'\W+')).where((i) => !i.isEmpty).forEach((String i) =>
-        result.update(i, (int value) => value + 1, ifAbsent: () => 1));
+    text.split(RegExp(r'[ ,\n!!&@$%^:]+')).where((i) => !i.isEmpty).forEach(
+        (String i) =>
+            result.update(i, (int value) => value + 1, ifAbsent: () => 1));
     return result;
   }
 }
