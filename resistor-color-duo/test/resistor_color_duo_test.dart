bc83973538722c29e55980aa249f754675af1a0e
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-24 21:17:17 +0000

    Working

--- resistor-color-duo/test/resistor_color_duo_test.dart
+++ resistor-color-duo/test/resistor_color_duo_test.dart
@@ -8,21 +8,21 @@ void main() {
     test("Brown and black", () {
       final int result = resistorColorDuo.value(["brown", "black"]);
       expect(result, equals(10));
-    }, skip: false);
+    });
 
     test("Blue and grey", () {
       final int result = resistorColorDuo.value(["blue", "grey"]);
       expect(result, equals(68));
-    }, skip: true);
+    });
 
     test("Yellow and violet", () {
       final int result = resistorColorDuo.value(["yellow", "violet"]);
       expect(result, equals(47));
-    }, skip: true);
+    });
 
     test("Orange and orange", () {
       final int result = resistorColorDuo.value(["orange", "orange"]);
       expect(result, equals(33));
-    }, skip: true);
+    });
   });
 }
