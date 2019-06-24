183ad724113c47bffbeedf33de1dd3eda807b7e7
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-24 18:15:11 +0000

    Working

--- space-age/lib/space_age.dart
+++ space-age/lib/space_age.dart
@@ -1,5 +1,12 @@
 final durations = const {
   "Earth": 1,
+  "Mercury": 0.2408467,
+  "Venus": 0.61519726,
+  "Mars": 1.8808158,
+  "Jupiter": 11.862615,
+  "Saturn": 29.447498,
+  "Uranus": 84.016846,
+  "Neptune": 164.79132,
 };
 const period = 31557600;
 
