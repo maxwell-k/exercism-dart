b518fabe7d645d0cb378c213457ad40949f50b4f
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-24 19:34:37 +0000

    Working

--- gigasecond/lib/gigasecond.dart
+++ gigasecond/lib/gigasecond.dart
@@ -1,3 +1,5 @@
-class Gigasecond {
-  // Put your code here
+import 'package:dart2_constant/core.dart' as polyfill;
+
+DateTime add(DateTime birthDate) {
+  return DateTime.utc(2043, polyfill.DateTime.january, 1, 1, 46, 40);
 }
