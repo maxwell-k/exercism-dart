550278eaaf837453a888102fd779b15e0867caf6
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-25 07:08:12 +0000

    Working

--- phone-number/lib/phone_number.dart
+++ phone-number/lib/phone_number.dart
@@ -2,7 +2,8 @@ class PhoneNumber {
   String clean(String input) {
     String result = input.replaceAll(RegExp(r'[() -.]+'), '');
     if (result.startsWith("1")) result = result.substring(1);
-    if (!RegExp(r'^[2-9][0-9]{2}[2-9][0-9]{6}$').hasMatch(result)) return null;
-    return result;
+    return RegExp(r'^[2-9][0-9]{2}[2-9][0-9]{6}$').hasMatch(result)
+        ? result
+        : null;
   }
 }
