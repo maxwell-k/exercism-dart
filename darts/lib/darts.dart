0fe941b1c202bc72006c20c1496327125db96c7e
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-25 07:18:55 +0000

    Working

--- darts/lib/darts.dart
+++ darts/lib/darts.dart
@@ -4,6 +4,6 @@ class Darts {
   int score(double x, double y) {
     final double distance = sqrt(x * x + y * y);
     if (distance > 10) return 0;
-    throw UnimplementedError;
+    else return 1;
   }
 }
