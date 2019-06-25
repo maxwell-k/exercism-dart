a90b1c17b1cf315f86e766b3b9169b3c5ce26ec0
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-25 07:25:16 +0000

    Refactor after looking at example

--- darts/lib/darts.dart
+++ darts/lib/darts.dart
@@ -3,11 +3,11 @@ import 'dart:math' show sqrt;
 class Darts {
   int score(double x, double y) {
     final double distance = sqrt(x * x + y * y);
-    if (distance <= 10 && distance > 5)
-      return 1;
-    else if (distance <= 5 && distance > 1)
+    if (distance <= 1)
+      return 10;
+    else if (distance <= 5)
       return 5;
-    else if (distance <= 1) return 10;
+    else if (distance <= 10) return 1;
     return 0;
   }
 }
