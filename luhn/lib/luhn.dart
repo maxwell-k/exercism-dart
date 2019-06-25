6d088bad70e379989a4e72a6eec1daaf236f6747
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-25 08:40:37 +0000

    Working

--- luhn/lib/luhn.dart
+++ luhn/lib/luhn.dart
@@ -2,10 +2,8 @@ class Luhn {
   bool valid(String number) {
     if (number.length <= 1) return false;
     List<int> digits = number.split("").map(int.parse).toList();
-    for (var i = digits.length - 2; i >= 0; i -= 2) {
-      digits[i] *= 2;
-      digits[i] %= 9;
-    }
+    for (var i = digits.length - 2; i >= 0; i -= 2)
+      digits[i] = digits[i] * 2 % 9;
     final total = digits.reduce((total, i) => total + i);
     return total % 10 == 0;
   }
