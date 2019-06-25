31a352c2828b3ab68fef661530cff7530c27a67b
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-25 07:50:17 +0000

    Refactor based on example

--- prime-factors/lib/prime_factors.dart
+++ prime-factors/lib/prime_factors.dart
@@ -1,10 +1,9 @@
 class PrimeFactors {
-  List<int> factors(int input) {
-    int wip = input;
+  List<int> factors(int value) {
     List<int> factors = [];
-    for (var i = 2; i <= wip; i++) {
-      while (wip % i == 0) {
-        wip ~/= i;
+    for (var i = 2; i <= value; i++) {
+      while (value % i == 0) {
+        value ~/= i;
         factors.add(i);
       }
     }
