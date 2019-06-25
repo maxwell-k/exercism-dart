0fb550daf8ee745afe89548358a8216ff709dab7
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-25 07:36:35 +0000

    Working

--- prime-factors/lib/prime_factors.dart
+++ prime-factors/lib/prime_factors.dart
@@ -1,8 +1,12 @@
 class PrimeFactors {
   List<int> factors(int input) {
+    int wip = input;
     List<int> factors = [];
     for (var i = 2; i <= input; i++) {
-      if (input % i == 0) factors.add(i);
+      if (wip % i == 0) {
+        wip ~/= i;
+        factors.add(i);
+      }
     }
     return factors;
   }
