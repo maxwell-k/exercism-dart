e63bfcec401a60c856048aae678b5ad87246da48
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-25 07:21:40 +0000

    Working

--- darts/lib/darts.dart
+++ darts/lib/darts.dart
@@ -3,7 +3,9 @@ import 'dart:math' show sqrt;
 class Darts {
   int score(double x, double y) {
     final double distance = sqrt(x * x + y * y);
-    if (distance > 10) return 0;
-    else return 1;
+    if (distance <= 10 && distance > 5)
+      return 1;
+    else if (distance <= 5) return 5;
+    return 0;
   }
 }
