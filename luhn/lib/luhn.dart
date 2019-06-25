315e4a2f956775767faf1154bb7f2b1f11072128
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-25 09:09:17 +0000

    Working

--- luhn/lib/luhn.dart
+++ luhn/lib/luhn.dart
@@ -8,10 +8,8 @@ class Luhn {
       digits.add(value);
     }
     if (digits.length <= 1) return false;
-    for (var i = digits.length - 2; i >= 0; i -= 2) {
-      digits[i] = digits[i] * 2;
-      if (digits[i] > 9) digits[i] -= 9;
-    }
+    for (var i = digits.length - 2; i >= 0; i -= 2) digits[i] = digits[i] * 2;
+    digits = digits.map((i) => i > 9 ? i - 9 : i).toList();
     return digits.reduce((total, element) => total + element) % 10 == 0;
   }
 }
