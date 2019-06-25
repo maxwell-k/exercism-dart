196da5032656cb7b19263b3aa1116f161cd7469e
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-25 07:01:40 +0000

    Working

--- phone-number/lib/phone_number.dart
+++ phone-number/lib/phone_number.dart
@@ -3,6 +3,7 @@ class PhoneNumber {
     String result = input.replaceAll(RegExp(r'[() -.]+'), '');
     if (result.startsWith("1")) result = result.substring(1);
     if (result.length != 10) return null;
+    if (!RegExp(r'[0-9]{10}').hasMatch(result)) return null;
     return result;
   }
 }
