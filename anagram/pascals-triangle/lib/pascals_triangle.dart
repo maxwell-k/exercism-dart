11b3cc4a22575b4b1ab950306e9b725325da9c1e
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-26 10:25:35 +0000

    Working

--- anagram/pascals-triangle/lib/pascals_triangle.dart
+++ anagram/pascals-triangle/lib/pascals_triangle.dart
@@ -1,9 +1,13 @@
 class PascalsTriangle {
+  List<int> _above(List<int> row) {
+    return [];
+  }
+
   List<List<int>> rows(int number) {
     if (number == 0) return [];
     List<List<int>> result = rows(number - 1);
     if (number == 1) result.add([1]);
-    if (number == 2) result.add([1] + [] + [1]);
+    if (number == 2) result.add([1] + _above(result.last) + [1]);
     if (number == 3) result.add([1] + [result.last[0] + result.last[1]] + [1]);
     if (number == 4)
       result.add([1] +
