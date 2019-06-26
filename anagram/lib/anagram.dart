345039bf572edbd800554a32dede9fed7c24f593
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-26 09:24:36 +0000

    Working

--- anagram/lib/anagram.dart
+++ anagram/lib/anagram.dart
@@ -1,6 +1,10 @@
 class Anagram {
   List<String> findAnagrams(String word, List<String> candidates) {
     final Set<String> letters = word.split("").toSet();
-    return candidates.where((i) => i.split("").toSet() == letters).toList();
+    print({'a'} == {'a'});
+    return candidates.where((candidate) {
+      Set<String> c = candidate.split("").toSet();
+      return c.length == letters.length && c.every(letters.contains);
+    }).toList();
   }
 }
--- anagram/test/anagram_test.dart
+++ anagram/test/anagram_test.dart
@@ -17,16 +17,18 @@ void main() {
 
 void simpleTests() {
   test("no matches", () {
-    final List<String> result = anagram.findAnagrams("diaper", ["hello", "world", "zombies", "pants"]);
+    final List<String> result =
+        anagram.findAnagrams("diaper", ["hello", "world", "zombies", "pants"]);
     expect(result, equals(<String>[]));
   }, skip: false);
 }
 
 void slightlyLessSimpleTests() {
   test("detects two anagrams", () {
-    final List<String> result = anagram.findAnagrams("master", ["stream", "pigeon", "maters"]);
+    final List<String> result =
+        anagram.findAnagrams("master", ["stream", "pigeon", "maters"]);
     expect(result, equals(["stream", "maters"]));
-  }, skip: true);
+  });
 
   test("does not detect anagram subsets", () {
     final List<String> result = anagram.findAnagrams("good", ["dog", "goody"]);
@@ -34,13 +36,14 @@ void slightlyLessSimpleTests() {
   }, skip: true);
 
   test("detects anagram", () {
-    final List<String> result = anagram.findAnagrams("listen", ["enlists", "google", "inlets", "banana"]);
+    final List<String> result = anagram
+        .findAnagrams("listen", ["enlists", "google", "inlets", "banana"]);
     expect(result, equals(["inlets"]));
   }, skip: true);
 
   test("detects three anagrams", () {
-    final List<String> result =
-        anagram.findAnagrams("allergy", ["gallery", "ballerina", "regally", "clergy", "largely", "leading"]);
+    final List<String> result = anagram.findAnagrams("allergy",
+        ["gallery", "ballerina", "regally", "clergy", "largely", "leading"]);
     expect(result, equals(["gallery", "regally", "largely"]));
   }, skip: true);
 }
@@ -52,17 +55,20 @@ void moreChallengingTests() {
   }, skip: true);
 
   test("detects anagrams case-insensitively", () {
-    final List<String> result = anagram.findAnagrams("Orchestra", ["cashregister", "Carthorse", "radishes"]);
+    final List<String> result = anagram
+        .findAnagrams("Orchestra", ["cashregister", "Carthorse", "radishes"]);
     expect(result, equals(["Carthorse"]));
   }, skip: true);
 
   test("detects anagrams using case-insensitive subject", () {
-    final List<String> result = anagram.findAnagrams("Orchestra", ["cashregister", "carthorse", "radishes"]);
+    final List<String> result = anagram
+        .findAnagrams("Orchestra", ["cashregister", "carthorse", "radishes"]);
     expect(result, equals(["carthorse"]));
   }, skip: true);
 
   test("detects anagrams using case-insensitive possible matches", () {
-    final List<String> result = anagram.findAnagrams("orchestra", ["cashregister", "Carthorse", "radishes"]);
+    final List<String> result = anagram
+        .findAnagrams("orchestra", ["cashregister", "Carthorse", "radishes"]);
     expect(result, equals(["Carthorse"]));
   }, skip: true);
 }
@@ -79,7 +85,8 @@ void edgeCaseTests() {
   }, skip: true);
 
   test("words are not anagrams of themselves (case-insensitive)", () {
-    final List<String> result = anagram.findAnagrams("BANANA", ["BANANA", "Banana", "banana"]);
+    final List<String> result =
+        anagram.findAnagrams("BANANA", ["BANANA", "Banana", "banana"]);
     expect(result, equals(<String>[]));
   }, skip: true);
 }
