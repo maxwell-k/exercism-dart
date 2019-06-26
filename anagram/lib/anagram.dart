f8a115649f061f02cace76b3eeabcaebd0d1702e
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-26 08:15:11 +0000

    Working

--- anagram/lib/anagram.dart
+++ anagram/lib/anagram.dart
@@ -1,6 +1,6 @@
 class Anagram {
   List<String> findAnagrams(String word, List<String> candidates) {
-    /* final Set<String> letters = word.split("").toSet(); */
-    return candidates.where((i) => false).toList();
+    final Set<String> letters = word.split("").toSet();
+    return candidates.where((i) => i.split("").toSet() == letters).toList();
   }
 }
