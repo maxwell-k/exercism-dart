55c8c567ae64cda8f25c2a67465b9d4559edc0b5
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-26 09:50:36 +0000

    Working

--- anagram/lib/anagram.dart
+++ anagram/lib/anagram.dart
@@ -1,13 +1,10 @@
-import 'package:collection/collection.dart' show ListEquality;
-
-const listEquality = ListEquality<String>();
-
 class Anagram {
   List<String> findAnagrams(String word, List<String> candidates) {
     List<String> letters = word.split("")..sort();
+    final sorted = letters.join("");
     return candidates.where((candidate) {
       List<String> candidate_letters = candidate.split("")..sort();
-      return listEquality.equals(letters, candidate_letters);
+      return candidate_letters.join("") == sorted;
     }).toList();
   }
 }
