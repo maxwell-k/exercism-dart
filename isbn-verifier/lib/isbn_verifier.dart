ba0669289301141dc5c8c1eed816f22c00c1a403
Keith Maxwell <keith.maxwell@gmail.com>
2019-06-26 11:06:05 +0000

    Working

--- isbn-verifier/lib/isbn_verifier.dart
+++ isbn-verifier/lib/isbn_verifier.dart
@@ -3,8 +3,10 @@ bool isValid(String isbn) {
   if (characters.length != 10) return false;
   if (characters[9] == 'X') characters[9] = '10';
 
-  int total =
-      Iterable<int>.generate(10, (i) => int.parse(characters[i]) * (10 - i))
-          .reduce((value, element) => value + element);
+  final List<int> digits = characters.map(int.tryParse).toList();
+  if (digits.any((i) => i == null)) return false;
+
+  int total = Iterable<int>.generate(10, (i) => digits[i] * (10 - i))
+      .reduce((value, element) => value + element);
   return total % 11 == 0;
 }
