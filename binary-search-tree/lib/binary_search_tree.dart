84393fcbc048ee664a2b9d11574a45e875317f24
Keith Maxwell <keith.maxwell@gmail.com>
2019-07-01 07:55:42 +0000

    Working

--- binary-search-tree/lib/binary_search_tree.dart
+++ binary-search-tree/lib/binary_search_tree.dart
@@ -1,3 +1,20 @@
+class Node {
+  String data;
+  Node left;
+  Node right;
+
+  Node(String this.data);
+}
+
 class BinarySearchTree {
-  // Put your code here
+  Node root;
+
+  BinarySearchTree(String data) {
+    this.root = Node(data);
+  }
+  void insert(String Data) {
+    throw UnimplementedError;
+  }
+
+  List<String> get sortedData => [];
 }
