25e0631f7f57e9ef2e19b7e7ba7d38ba7fe717d2
Keith Maxwell <keith.maxwell@gmail.com>
2019-07-08 22:10:03 +0000

    Working

--- minesweeper/lib/minesweeper.dart
+++ minesweeper/lib/minesweeper.dart
@@ -5,5 +5,8 @@ class Minesweeper {
     return this.input;
   }
 
+  int get rows => this.input?.length ?? 0;
+  int get columns => this.rows > 0 ? input[0].length : 0;
+
   const Minesweeper([List<String> this.input]);
 }
