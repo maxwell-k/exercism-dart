289e100109393d6e3a4c40e4f161c670ac25c885
Keith Maxwell <keith.maxwell@gmail.com>
2019-07-08 22:12:37 +0000

    Working

--- minesweeper/lib/minesweeper.dart
+++ minesweeper/lib/minesweeper.dart
@@ -1,12 +1,12 @@
 class Minesweeper {
   final List<String> input;
 
-  List<String> get annotated {
-    return this.input;
-  }
-
   int get rows => this.input?.length ?? 0;
   int get columns => this.rows > 0 ? input[0].length : 0;
+
+  List<String> get annotated {
+    return this.input.map((i) => i).toList();
+  }
 
   const Minesweeper([List<String> this.input]);
 }
