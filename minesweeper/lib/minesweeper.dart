eca613a38d11e669a30002094157857e8fef58bc
Keith Maxwell <keith.maxwell@gmail.com>
2019-07-08 22:29:38 +0000

    Working

--- minesweeper/lib/minesweeper.dart
+++ minesweeper/lib/minesweeper.dart
@@ -1,15 +1,29 @@
 bool mine(String input) => input == '*';
 
+class Offset {
+  final int x, y;
+  const Offset(this.x, this.y);
+}
+
+final List<Offset> offsets = const [
+  Offset(-1, -1),
+  Offset(-1, 0),
+  Offset(-1, 1),
+  Offset(0, -1),
+  Offset(0, 1),
+  Offset(1, 1),
+  Offset(1, 0),
+  Offset(1, -1),
+];
+
 class Minesweeper {
   final List<String> input;
 
   int get rows => this.input?.length ?? 0;
   int get columns => this.rows > 0 ? input[0].length : 0;
 
-  // List<Point> neighbours(int x, int y) {
-  //   return [];
-  // }
   String value(int x, int y) {
+    if (mine(this.input[y][x])) return this.input[y][x];
     return this.input[y][x];
   }
 
