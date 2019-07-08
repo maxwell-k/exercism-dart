cb0a59762a7181e9d604dbe7ba579c6759dac0a2
Keith Maxwell <keith.maxwell@gmail.com>
2019-07-08 22:06:35 +0000

    Working

--- minesweeper/lib/minesweeper.dart
+++ minesweeper/lib/minesweeper.dart
@@ -1,18 +1,9 @@
 class Minesweeper {
-  List<String> annotated;
+  List<String> input;
 
-  Minesweeper([List<String> input]) {
-    var buffer = StringBuffer();
-    this.annotated = List<String>();
-
-    final rows = input?.length ?? 0;
-    final columns = rows > 0 ? input[0].length : 0;
-    for (var i = 0; i < rows; i++) {
-      for (var j = 0; j < columns; j++) {
-        buffer.write(input[i][j]);
-      }
-      this.annotated.add(buffer.toString());
-      buffer.clear();
-    }
+  List<String> get annotated {
+    return this.input;
   }
+
+  Minesweeper([List<String> this.input]);
 }
