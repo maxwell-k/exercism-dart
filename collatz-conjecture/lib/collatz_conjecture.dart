644ea810c4704b6be3119873158b5b35359af2d7
Keith Maxwell <keith.maxwell@gmail.com>
2019-07-09 08:16:29 +0000

    Working

--- collatz-conjecture/lib/collatz_conjecture.dart
+++ collatz-conjecture/lib/collatz_conjecture.dart
@@ -4,11 +4,10 @@ class CollatzConjecture {
   int steps(int n) {
     int count = 0;
     while (n != 1) {
-      if (n % 2 == 0) {
+      if (n % 2 == 0)
         n ~/= 2;
-      } else {
+      else
         n = 3 * n + 1;
-      }
       count++;
     }
     return count;
