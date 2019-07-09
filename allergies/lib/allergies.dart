9a589fcdfeec516b1a675acc97041b703a574445
Keith Maxwell <keith.maxwell@gmail.com>
2019-07-09 17:27:38 +0000

    Working

--- allergies/lib/allergies.dart
+++ allergies/lib/allergies.dart
@@ -11,11 +11,13 @@ enum Allergy {
 
 String describe(Allergy i) => i.toString().split('.').last;
 
+bool check(int score, int index) => (score >> index) % 2 == 1;
+
 class Allergies {
   List<String> allergies = Allergy.values.map(describe).toList();
 
   bool allergicTo(String item, int score) =>
-      (score >> this.allergies.indexOf(item)) % 2 == 1;
+      check(score, this.allergies.indexOf(item));
 
   List<String> list(int score) {
     return Allergy.values
