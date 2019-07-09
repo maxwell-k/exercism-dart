f7a3ff6a071b6abc77b7d075cbc7349591525853
Keith Maxwell <keith.maxwell@gmail.com>
2019-07-09 17:13:52 +0000

    Working

--- allergies/lib/allergies.dart
+++ allergies/lib/allergies.dart
@@ -10,12 +10,14 @@ enum Item {
 }
 
 class Allergies {
-  Map<String, int> items = Map<String, int>.fromIterable(Item.values,
-      key: (dynamic i) => i.toString().split('.').last,
-      value: (dynamic i) => 1 << (i.index as int));
+  /* Map<String, int> items = Map<String, int>.fromIterable(Item.values, */
+  /*     key: (dynamic i) => i.toString().split('.').last, */
+  /*     value: (dynamic i) => 1 << (i.index as int)); */
+  List<String> items =
+      Item.values.map((i) => i.toString().split('.').last).toList();
 
   bool allergicTo(String item, int score) {
-    return score >= this.items[item];
+    return (score >> this.items.indexOf(item)) % 2 == 1;
   }
 
   List<String> list(int score) => List<String>();
