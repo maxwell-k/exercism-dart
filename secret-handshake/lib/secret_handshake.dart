5eca2b99011f4a8f38a4cb57a4633114771044ca
Keith Maxwell <keith.maxwell@gmail.com>
2019-07-09 21:20:22 +0000

    Refactor after looking at example

--- secret-handshake/lib/secret_handshake.dart
+++ secret-handshake/lib/secret_handshake.dart
@@ -1,10 +1,10 @@
+final events = const ['wink', 'double blink', 'close your eyes', 'jump'];
+
 class SecretHandshake {
   List<String> commands(int message) {
-    var output = List<String>();
-    if (message & 1 > 0) output.add('wink');
-    if (message & 2 > 0) output.add('double blink');
-    if (message & 4 > 0) output.add('close your eyes');
-    if (message & 8 > 0) output.add('jump');
+    List<String> output = List<String>();
+    for (int i = 0; i < events.length; i++)
+      if (message & (1 << i) > 0) output.add(events[i]);
     return (message & 16 > 0) ? output.reversed.toList() : output;
   }
 }
