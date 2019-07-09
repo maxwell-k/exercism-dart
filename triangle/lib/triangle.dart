4ee580b36432b205f22e22bc138e9990b282df8c
Keith Maxwell <keith.maxwell@gmail.com>
2019-07-09 18:11:17 +0000

    Refactor based on example

--- triangle/lib/triangle.dart
+++ triangle/lib/triangle.dart
@@ -1,10 +1,12 @@
 class Triangle {
-  bool _inequality(num a, num b, num c) =>
-      a < (b + c) && b < (a + c) && c < (a + b);
+  bool _valid(num a, num b, num c) =>
+      {a, b, c}.every((l) => l > 0) &&
+      a < (b + c) &&
+      b < (a + c) &&
+      c < (a + b);
   bool equilateral(num a, num b, num c) =>
-      _inequality(a, b, c) && {a, b, c}.length == 1 && a != 0;
+      _valid(a, b, c) && {a, b, c}.length == 1;
   bool isosceles(num a, num b, num c) =>
-      _inequality(a, b, c) && {a, b, c}.length <= 2;
-  bool scalene(num a, num b, num c) =>
-      _inequality(a, b, c) && {a, b, c}.length == 3;
+      _valid(a, b, c) && {a, b, c}.length <= 2;
+  bool scalene(num a, num b, num c) => _valid(a, b, c) && {a, b, c}.length == 3;
 }
