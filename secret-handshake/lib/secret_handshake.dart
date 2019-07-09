f21d16298c20342b0626e9c31d96ea99a7a88697
Keith Maxwell <keith.maxwell@gmail.com>
2019-07-09 21:21:36 +0000

    Refactor after looking at example

--- secret-handshake/lib/secret_handshake.dart
+++ secret-handshake/lib/secret_handshake.dart
@@ -4,7 +4,7 @@ class SecretHandshake {
   List<String> commands(int message) {
     List<String> output = List<String>();
     for (int i = 0; i < events.length; i++)
-      if (message & (1 << i) > 0) output.add(events[i]);
+      if (message & 1 << i > 0) output.add(events[i]);
     return (message & 1 << events.length > 0)
         ? output.reversed.toList()
         : output;
