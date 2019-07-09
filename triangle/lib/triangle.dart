7757d6918cdb761f045d77cb46434feebe379630
Keith Maxwell <keith.maxwell@gmail.com>
2019-07-09 18:02:26 +0000

    Working

--- triangle/lib/triangle.dart
+++ triangle/lib/triangle.dart
@@ -1,5 +1,7 @@
 class Triangle {
+  bool _valid(num a, num b, num c) => a < (b + c);
   bool equilateral(num a, num b, num c) => {a, b, c}.length == 1 && a != 0;
-  bool isosceles(num a, num b, num c) => {a, b, c}.length <= 2;
+  bool isosceles(num a, num b, num c) =>
+      _valid(a, b, c) && {a, b, c}.length <= 2;
   bool scalene(num a, num b, num c) => true;
 }
