492ec27cb60b0018dc0e10f0696987554ccd6c53
Keith Maxwell <keith.maxwell@gmail.com>
2019-07-09 21:14:04 +0000

    Working

--- secret-handshake/lib/secret_handshake.dart
+++ secret-handshake/lib/secret_handshake.dart
@@ -1,13 +1,10 @@
 class SecretHandshake {
   List<String> commands(int message) {
-    List<String> output = List<String>();
+    var output = List<String>();
     if (message & 1 > 0) output.add('wink');
     if (message & 2 > 0) output.add('double blink');
     if (message & 4 > 0) output.add('close your eyes');
     if (message & 8 > 0) output.add('jump');
-    if (message & 16 > 0)
-      return output.reversed.toList();
-    else
-      return output;
+    return (message & 16 > 0) ? output.reversed.toList() : output;
   }
 }
