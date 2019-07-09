836e78a89236db938a35334478c293464871565b
Keith Maxwell <keith.maxwell@gmail.com>
2019-07-09 17:04:01 +0000

    Working

--- allergies/lib/allergies.dart
+++ allergies/lib/allergies.dart
@@ -1,4 +1,22 @@
+import 'dart:math' show pow;
+
+enum Item {
+  eggs,
+  peanuts,
+  shellfish,
+  strawberries,
+  tomatoes,
+  chocolate,
+  pollen,
+  cats,
+}
+
 class Allergies {
-  bool allergicTo(String item, int score) => false;
+  Map<String, int> items = Map<String, int>.fromIterable(Item.values,
+      key: (dynamic i) => i.toString().split('.').last,
+      value: (dynamic i) => pow(2, i.index as num) as int);
+
+  bool allergicTo(String item, int score) => score > this.items[item];
+
   List<String> list(int score) => List<String>();
 }
